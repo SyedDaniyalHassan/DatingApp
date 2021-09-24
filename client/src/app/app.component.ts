7e00a7aff35396ef50ea9815c7866f1d42dbda4f
SyedDaniyalHassan <daniyalhassan11081999@gmail.com>
2021-09-24 21:30:29 +0500

    Add Cors in backend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+
+  constructor(private http:HttpClient)
+  {
+    //constructor
+  }
+
+  ngOnInit() {
+    this.httpGet()
+  }
+  title = 'The Dating App!';
+  name="daniyal";
+  id :any ;
+  User:any;
+
+  httpGet()
+  {
+    this.http.get("https://localhost:5001/api/users/1").subscribe(res =>{
+      console.log("get the response");
+      this.User = res;
+      console.log(res);
+      
+    },err=>{
+      console.log(err);
+    });
+    
+  }
 }
